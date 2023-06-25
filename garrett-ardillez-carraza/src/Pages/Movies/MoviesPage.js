b872c2af6ee7aa375e9b80ab1daf8f53157c967b
Garrett Christian <christiangarrett05@gmail.com>
2023-06-25 11:00:39 -0300

    2506231100

--- garrett-ardillez-carraza/src/Pages/Movies/MoviesPage.js
+++ garrett-ardillez-carraza/src/Pages/Movies/MoviesPage.js
@@ -1,6 +1,12 @@
 import { Header } from "../../Components/Header/Header"
+import { useEffect , useState } from "react"
+
+const episode= "https://rickandmortyapi.com/api/episode"
 
 export const MoviesPage = () =>{
+
+
+
     return(
         <div>
             <Header/>
